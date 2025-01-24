7b0c46defacdb1fdad7a7ece26ddf0114265aaca
ErykPoczykowski <147150382+ErykPoczykowski@users.noreply.github.com>
2025-01-24 15:17:16 +0100

    Update Zestaw9_0.java

--- zestaw9/Zestaw9_0.java
+++ zestaw9/Zestaw9_0.java
@@ -1,3 +1,102 @@
+class Vehicle {
+    protected String brand;
+    protected String model;
+
+    public Vehicle(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+    }
+}
+
+class Car extends Vehicle {
+    private int numberOfDoors;
+
+    public Car(String brand, String model, int numberOfDoors) {
+        super(brand, model);
+        this.numberOfDoors = numberOfDoors;
+    }
+}
+
+
+abstract class WorkTool {
+    protected String name;
+    protected int productionYear;
+
+    public WorkTool(String name, int productionYear) {
+        this.name = name;
+        this.productionYear = productionYear;
+    }
+
+    public abstract void use();
+}
+
+class Hammer extends WorkTool {
+    public Hammer(String name, int productionYear) {
+        super(name, productionYear);
+    }
+
+    @Override
+    public void use() {
+        System.out.println("Hammering!");
+    }
+}
+
+class Screwdriver extends WorkTool {
+    public Screwdriver(String name, int productionYear) {
+        super(name, productionYear);
+    }
+
+    @Override
+    public void use() {
+        System.out.println("Screwing!");
+    }
+}
+
+class Saw extends WorkTool {
+    public Saw(String name, int productionYear) {
+        super(name, productionYear);
+    }
+
+    @Override
+    public void use() {
+        System.out.println("Sawing!");
+    }
+}
+
+
+interface Sensor {
+    double readValue();
+    String getStatus();
+    void reset();
+}
+
+class TemperatureSensor implements Sensor {
+    @Override
+    public double readValue() {
+        return 22.5;  // Example value
+    }
+
+    @Override
+    public String getStatus() {
+        return "Temperature Sensor is active";
+    }
+
+    @Override
+    public void reset() {
+        System.out.println("Temperature Sensor reset");
+    }
+}
+
+class PressureSensor implements Sensor {
+    @Override
+    public double readValue() {
+        return 101.3;  // Example value
+    }
+
+    @Override
+    public String getStatus() {
+        return "Pressure Sensor is active";
+    }
 
     @Override
     public void reset() {
